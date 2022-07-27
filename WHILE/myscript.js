040930d0f8af2e785fca9dee4f6f5ea40fb6e6da
Leonomics <leocv97@gmail.com>
2022-07-27 15:32:08 +0200

    fix while condition

--- WHILE/myscript.js
+++ WHILE/myscript.js
@@ -3,7 +3,7 @@ listaSpesa = ["pane", "uova", "pasta", "latte", "gelato", "mele", "pollo"];
 let listElementsContainer = document.getElementById("listElementsContainer");
 let i = 0;
 
-while(i <= listaSpesa.length)	{
+while(i < listaSpesa.length)	{
 	element = listaSpesa[i];
 	const li = `
 	<li class="slide">
