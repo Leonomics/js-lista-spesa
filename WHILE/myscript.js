d414cf2863a2510a57473f3263a0ad408c742272
Leonomics <leocv97@gmail.com>
2022-07-27 14:38:35 +0200

    add list to html using while loop

--- WHILE/myscript.js
+++ WHILE/myscript.js
@@ -1,7 +1,15 @@
 listaSpesa = ["pane", "uova", "pasta", "latte", "gelato", "mele", "pollo"];
 
 let listElementsContainer = document.getElementById("listElementsContainer");
+let i = 0;
 
-while()	{
-	
+while(i <= listaSpesa.length)	{
+	element = listaSpesa[i];
+	const li = `
+	<li class="slide">
+		<p>${element}<p>
+	</li>
+	`
+	listElementsContainer.innerHTML += li;
+	i++;
 }
